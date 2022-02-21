2d0d7e314a239bff451ea4a6da53b255e563a0ee
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-02-21 23:14:18 +0100

    Example project for source gen bug

---
+++ Project/Foo.axaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace Project;
+
+public partial class Foo : UserControl
+{
+    public Foo()
+    {
+        InitializeComponent();
+        
+        Console.WriteLine(Foobar.Text);
+        Console.WriteLine(Foobar2.Text);
+    }
+}
---
+++ Project/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Project
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
